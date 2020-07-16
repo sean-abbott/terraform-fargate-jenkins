6ffb5df87b63d2d7c3ef5b8fd8cd7508009896c9
sabbott <sabbott@cargurus.com>
2020-07-16 16:21:04 -0400

    task definition works

--- main.tf
+++ main.tf
@@ -1,19 +1,84 @@
+locals {
+  server_healthcheck = {
+    command     = ["CMD-SHELL", "curl -f http://localhost:8080 || exit 1"]
+    retries     = 3
+    timeout     = 5
+    interval    = 30
+    startPeriod = 120
+  }
+  server_td_port_mappings = [
+    {
+      containerPort = 8080
+      hostPort      = 8080
+      protocol      = "tcp"
+    },
+    {
+      containerPort = 50000
+      hostPort      = 50000
+      protocol      = "tcp"
+    }
+  ]
+  server_environment = [
+    {
+      name  = "CASC_JENKINS_CONFIG",
+      value = var.jenkins_config_url
+    }
+  ]
+}
 #------------------------------------------------------------------------------
 # AWS ECS Task Execution Role
 #------------------------------------------------------------------------------
+data "aws_iam_policy_document" "ecs_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "${var.name_preffix}-ecs-task-execution-role"
-  assume_role_policy = file("${path.module}/files/iam/ecs_task_execution_iam_role.json")
+  name               = "${var.name_prefix}-ecs-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attach" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+# End task execution role
+
+module "container_definition" {
+  # https://github.com/cloudposse/terraform-aws-ecs-container-definition
+  source  = "cloudposse/ecs-container-definition/aws"
+  version = "0.23.0"
+
+  container_name               = "${var.name_prefix}-jenkins-server"
+  container_image              = "jenkins/jenkins:lts"
+  container_memory             = 4096
+  container_memory_reservation = 2048
+  port_mappings                = local.server_td_port_mappings
+  healthcheck                  = local.server_healthcheck
+  container_cpu                = 2048
+  essential                    = true
+  environment                  = local.server_environment
+  # TODO pass in log config
+  #log_configuration            = var.log_configuration
+  # TODO efs
+  #mount_points                 = var.mount_points
+  # TODO this is also a possibility if git source doesn't work
+  #volumes_from                 = var.volumes_from
+  # TODO this is a possibility if git source doesn't work
+  #container_depends_on         = var.container_depends_on
+}
 
 # Task Definition
 resource "aws_ecs_task_definition" "server" {
-  family                = "jenkins-server"
+  family = "jenkins-server"
+  # TODO looks ok, mostly just a wrapper for the map
+  # https://github.com/cloudposse/terraform-aws-ecs-container-definition/blob/master/main.tf#L27
   container_definitions = "[ ${module.container_definition.json_map} ]" # TODO
   task_role_arn         = var.task_role_arn == null ? aws_iam_role.ecs_task_execution_role.arn : var.task_role_arn
   execution_role_arn    = aws_iam_role.ecs_task_execution_role.arn
--- variables.tf
+++ variables.tf
@@ -1,8 +1,18 @@
+# Required variables
+variable "jenkins_config_url" {
+  description = "This can be any url that the server can access, like a github raw url"
+  type        = string
+}
+variable "name_prefix" {
+  description = "prefix for various resource names in the module"
+  type        = string
+}
+
 # Optional variables
 variable "container_cpu" {
   # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/AWS_Fargate.html#fargate-task-defs
   description = "(Optional) The number of cpu units to reserve for the container. This is optional for tasks using Fargate launch type and the total amount of container_cpu of all containers in a task will need to be lower than the task-level cpu value"
-  default     = 1024 # 1 vCPU
+  default     = 2048 # 2 vCPU
 }
 
 variable "container_memory" {
@@ -15,4 +25,24 @@ variable "task_role_arn" {
   description = "(Optional) The ARN of IAM role that allows your Amazon ECS container task to make calls to other AWS services. If not specified, `aws_iam_role.ecs_task_execution_role.arn` is used"
   type        = string
   default     = null
+}
+
+variable "volumes" {
+  description = "(Optional) A set of volume blocks that containers in your task may use"
+  type = list(object({
+    host_path = string
+    name      = string
+    docker_volume_configuration = list(object({
+      autoprovision = bool
+      driver        = string
+      driver_opts   = map(string)
+      labels        = map(string)
+      scope         = string
+    }))
+    efs_volume_configuration = list(object({
+      file_system_id = string
+      root_directory = string
+    }))
+  }))
+  default = []
 }
